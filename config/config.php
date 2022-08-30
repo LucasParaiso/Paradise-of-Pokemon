7a8574ceab659d6f090a88a6e14dc79b51cfe2da
Lucas Paraiso <62018977+lucas1001lucas@users.noreply.github.com>
2022-08-30 04:43:24 -0300

    Dinamic url

--- config/config.php
+++ config/config.php
@@ -2,14 +2,14 @@
 
 function pokemons()
 {
-    $url = "http://localhost:8000/api/pokemon.php/";
+    $url = "https://" . $_SERVER['SERVER_NAME'] . "/api/pokemon.php/";
 
     return json_decode(file_get_contents($url));
 }
 
 function pokemon($pokeNumber)
 {
-    $baseUrl = "http://localhost:8000/api/pokemon.php/";
+    $baseUrl = "https://" . $_SERVER['SERVER_NAME'] . "/api/pokemon.php/";
 
     $url = $baseUrl . '?id=' . $pokeNumber;
 
