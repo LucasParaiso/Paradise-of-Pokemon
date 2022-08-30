9220856bdf8ebfde9d2ded7860665c95ced55883
Lucas Paraiso <62018977+lucas1001lucas@users.noreply.github.com>
2022-08-30 05:50:59 -0300

    config add form

--- config/config.php
+++ config/config.php
@@ -2,16 +2,50 @@
 
 function pokemons()
 {
-    $url = "https://" . $_SERVER['SERVER_NAME'] . "/api/pokemon.php/";
+    $url = 'https://pokeapi.co/api/v2/pokemon?limit=150';
+    $pokemonInfo = new stdClass();
 
-    return json_decode(file_get_contents($url));
+    if ($conteudo = file_get_contents($url)) {
+        $pokemons = json_decode($conteudo);
+
+        foreach ($pokemons->results as $key => $pokemon) {
+            $pokeNumber = $key + 1;
+            $pokeName = $pokemon->name;
+            $pokeImage = 'https://img.pokemondb.net/artwork/large/' . $pokeName . '.jpg';
+
+            $pokemonInfo->$pokeNumber = [
+                'name' => $pokeName,
+                'img' => $pokeImage
+            ];
+        }
+    }
+
+    $temp = json_encode($pokemonInfo);
+    return json_decode($temp);
 }
 
 function pokemon($pokeNumber)
 {
-    $baseUrl = "https://" . $_SERVER['SERVER_NAME'] . "/api/pokemon.php/";
+    $url = 'https://pokeapi.co/api/v2/pokemon/' . $pokeNumber;
 
-    $url = $baseUrl . '?id=' . $pokeNumber;
+    $pokemonInfo = new stdClass();
+    $pokemonInfo->error = true;
+    $pokemonInfo->id = null;
+    $pokemonInfo->name = null;
+    $pokemonInfo->img = null;
 
-    return json_decode(file_get_contents($url));
+    if ($pokeNumber <= 150) {
+        $pokemon = json_decode(file_get_contents($url));
+
+        $pokeName = $pokemon->name;
+        $pokeImage = 'https://img.pokemondb.net/artwork/large/' . $pokeName . '.jpg';
+
+        $pokemonInfo->error = false;
+        $pokemonInfo->id = $pokeNumber;
+        $pokemonInfo->name = $pokeName;
+        $pokemonInfo->img = $pokeImage;
+    }
+    
+    $temp = json_encode($pokemonInfo);
+    return json_decode($temp);
 }
