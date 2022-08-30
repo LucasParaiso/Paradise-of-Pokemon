3a06450836c7761f92d08d5c3d0edab52b945aa4
Lucas Paraiso <62018977+lucas1001lucas@users.noreply.github.com>
2022-08-30 04:54:35 -0300

    card.php updated

--- views/card.php
+++ views/card.php
@@ -1,10 +1,11 @@
-<h1 class="m-5 mb-0">#<?= $_POST['pokemonId']?> pokemon in pokedex:</h1>
+<?php
+    $pokemon = isset($_POST['pokemonId']) ? $_POST['pokemonId'] : null;
+?>
+
+<h1 class="m-5 mb-0">#<?= $pokemon->id ?> pokemon in pokedex:</h1>
 
 <div class="container text-center">
     <div class="row row-cols-1 row-cols-sm-3 p-5 m-5 mt-0   justify-content-center">
-        <?php
-        $pokemon = pokemon($_POST['pokemonId']);
-        ?>
         <div class='col my-2'>
             <div class='card border-1'>
                 <img src='<?= $pokemon->img ?>' class='card-img-top' alt='<?= $pokemon->name ?>'>
